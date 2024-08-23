9604b1c6655535fa7aca3c28ae5309853bc47061
Konan <kixeyems0@gmail.com>
2024-08-23 15:44:24 +0100

    ui setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Konan's Portfolio",
+    default: "Konan's Portfolio",
+  },
+  description: "Konan's Portfolio, with a custom chatbot",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/favicon.ico",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <main className="mx-auto max-w-3xl py-10 px-3">{children}</main>
+      </body>
+    </html>
+  );
+}
